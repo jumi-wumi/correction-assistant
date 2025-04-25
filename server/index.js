40343e5063fe0dfe30ab12f200b37f54d36e568b
Jumi <feliciastrandberg@gmail.com>
2025-04-25 11:52:15 +0200

    Imports and configures dotenv module in index. Changes test route accordingly.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors"; 
+import dotenv from "dotenv"
 
 const app = express(); 
 
+dotenv.config(); 
+
 app.use(cors());
 
-app.listen(3030, () => {
-    console.log("Server started on port 3030")
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`)
 });
 
 // Test route
