f30140c0494f1cce17648760f77a44bed9133ff7
Jumi <feliciastrandberg@gmail.com>
2025-04-25 11:31:15 +0200

    Installs CORS middleware library

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express(); 
 
-app.listen(3000, () => {
+app.listen(3030, () => {
     console.log("Server started on port 3030")
+});
+
+// Test route
+app.get("/", (request, response) => {
+    response.send("Hiiiiiii bestie! <3");
 });
