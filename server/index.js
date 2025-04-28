29d9da32092aef6cc86365c0cafc3664ffa7bde4
Jumi <feliciastrandberg@gmail.com>
2025-04-28 11:40:04 +0200

    Comments out test section. Keep for future.

--- server/index.js
+++ server/index.js
@@ -13,28 +13,31 @@ app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`)
 });
 
-// Test route
-app.get("/", (request, response) => {
-    response.send("Hiiiiiii bestie! <3");
-});
+//----------TEST SECTION! UNCOMMENT TO TEST ROUTE AND/OR API----------
+// // Test route
+// app.get("/", (request, response) => {
+//     response.send("Hiiiiiii bestie! <3");
+// });
 
-// Init OpenAi client with our key
-const openai = new OpenAI({
-    apiKey: process.env.OPEN_AI_KEY,
-});
+// // Init OpenAi client with our key
+// const openai = new OpenAI({
+//     apiKey: process.env.OPEN_AI_KEY,
+// });
 
-// OpenAI test
-app.get("/test", async(request, response) => {
-    console.log("request received on /test")
+// // OpenAI test
+// app.get("/test", async(request, response) => {
+//     console.log("request received on /test")
 
-    try{
-        const modelResponse = await openai.responses.create({
-            model: "gpt-4.1",
-            input: "Ge mig ett coolt djur-fakta"
-        }); 
+//     try{
+//         const modelResponse = await openai.responses.create({
+//             model: "gpt-4.1",
+//             input: "Ge mig ett coolt djur-fakta"
+//         }); 
 
-        response.json(modelResponse)
-    } catch(error) {
-        console.error(error); 
-    }
-});
+//         response.json(modelResponse)
+//     } catch(error) {
+//         console.error(error); 
+//     }
+// });
+//----------END TEST SECTION----------
+
