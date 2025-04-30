e3e47ecc49212ab289d020caf6f09342522961d7
Jumi <feliciastrandberg@gmail.com>
2025-04-30 14:31:58 +0200

    Adds textarea elements for all input needed by the user (prompt, description, assignment)

--- client/src/components/Assistant.jsx
+++ client/src/components/Assistant.jsx
@@ -7,8 +7,35 @@ const Assistant = () => {
   const [description, setDescription] = useState("");
   // Init the default response results as object
   const [results, setResults] = useState({});
+  // Init the default assignment text as empty
+  const [assignment, setAssignment] = useState("");
 
-  return <div></div>;
+  return (
+    <div className="assistant-container">
+      <h1>Bajsa på dig</h1>
+      // Input for the prompt
+      <textarea
+        placeholder="Vad kan jag hjälpa dig med?"
+        value="{prompt}"
+        onChange={(event) => setPrompt(event.target.value)}
+      ></textarea>
+
+      // Input for uppgiftsbeskrivning
+      <textarea
+        placeholder="Uppgiftsbeskrivning?"
+        value="{description}"
+        onChange={(event) => setDescription(event.target.value)}
+      ></textarea>
+      
+      //Input for the assignment
+      <textarea
+        placeholder="Klistra in uppgiften som ska bedömas"
+        value="{assignment}"
+        onChange={(event) => setAssignment(event.target.value)}
+      ></textarea>
+      
+    </div>
+  );
 };
 
 export default Assistant;
