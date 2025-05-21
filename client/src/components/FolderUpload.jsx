34a5e6d415b4b50c9f2590507d91b1a8fa1c2f38
Jumi <feliciastrandberg@gmail.com>
2025-05-21 13:10:42 +0200

    Changes input to accept folder (only works in Chromium based browsers. Cannot accept only files now.

--- client/src/components/FolderUpload.jsx
+++ client/src/components/FolderUpload.jsx
@@ -1,34 +1,43 @@
 import { use, useState } from "react";
 
 const FolderUpload = () => {
-    const [files, setFiles] = useState([]); 
-    const [uploadedFiles, setUploadedFiles] = useState([]); 
+  const [files, setFiles] = useState([]);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
 
-    const handleFileChange = (event) => {
-        setFiles(event.target.files); 
-    }; 
+  const handleFileChange = (event) => {
+    setFiles(event.target.files);
+  };
 
-    const handleUpload = async () => {
-        const formData = new FormData(); 
+  const handleUpload = async () => {
+    const formData = new FormData();
 
-        // append files to formdata
-        for (let i = 0; i < files.length; i++) {
-            formData.append("files", files[i]); 
-        }
+    // append files to formdata
+    for (let i = 0; i < files.length; i++) {
+      formData.append("files", files[i]);
+    }
 
-        const response = await fetch("http://localhost:3000/upload-from-folder", {
-            method: "POST", 
-            body: formData
-        }); 
+    const response = await fetch("http://localhost:3000/upload-from-folder", {
+      method: "POST",
+      body: formData,
+    });
 
-        if (!response.ok) throw new Error("failed to upload files");
+    if (!response.ok) throw new Error("failed to upload files");
 
-        const data = await response.json(); 
-        setUploadedFiles(data.files);
-    };
+    const data = await response.json();
+    setUploadedFiles(data.files);
+  };
 
-  return <>
-      <input type="file" multiple onChange={handleFileChange} />
+  return (
+    <>
+      <input
+        type="file"
+        // OBS! Only handle folder uploads in Chromium based browsers
+        webkitdirectory="true"
+        directory="true"
+        multiple
+        onChange={(e) => setFiles(e.target.files)}
+      />
+
       <button onClick={handleUpload}>Upload Files</button>
 
       {uploadedFiles.length > 0 && (
@@ -51,7 +60,8 @@ const FolderUpload = () => {
           </tbody>
         </table>
       )}
-  </>;
+    </>
+  );
 };
 
 export default FolderUpload;
