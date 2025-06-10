b0b18131d91d57c0b78d28e4d545dfe09619f9c4
Jumi <feliciastrandberg@gmail.com>
2025-06-10 12:09:33 +0200

    Changes filename format to save on disc to make each file unique

--- server/routes/upload-unzipped.js
+++ server/routes/upload-unzipped.js
@@ -1,35 +1,46 @@
-import express from "express"; 
+import express from "express";
 import multer from "multer";
+import crypto from "crypto";
+import path from "path";
 
-const router = express.Router(); 
+const router = express.Router();
 
 const fileStorage = multer.diskStorage({
   destination: (request, file, cb) => {
     cb(null, "uploads/");
   },
   filename: (request, file, cb) => {
-    cb(null, `${file.originalname}`); 
-  }
+    const timestamp = Date.now();
+    // generates a random UUID
+    const uuid = crypto.randomUUID();
+    // get .pdf extension
+    const extension = path.extname(file.originalname); 
+
+    const safeFilename = `${timestamp}-${uuid}${extension}`;
+    cb(null, safeFilename);
+  },
 });
 
-const upload = multer({ storage: fileStorage }); 
+const upload = multer({ storage: fileStorage });
 
+router.post(
+  "/upload-from-folder",
+  upload.array("files"),
+  async (request, response) => {
+    try {
+      const uploadedFiles = request.files.map((file) => {
+        return {
+          filename: file.originalname,
+          url: `/uploads/${file.filename}`,
+        };
+      });
 
-router.post("/upload-from-folder", upload.array("files"), async (request, response) => {
-  try {
-    const uploadedFiles = request.files.map((file) => {
-      return {
-        filename: file.originalname,
-        url: `/uploads/${file.filename}`,
-      };
-    });
-
-    response.json({ files: uploadedFiles });
-  } catch (err) {
-    console.error(err);
-    response.status(500).json({ error: "File processing failed" });
+      response.json({ files: uploadedFiles });
+    } catch (err) {
+      console.error(err);
+      response.status(500).json({ error: "File processing failed" });
+    }
   }
-});
+);
 
-
-export default router
+export default router;
